6199bfa00215257a119ec4cfd5b21d402083e985
aleks-shar <l-e-stat@yandex.ru>
2023-05-16 20:24:06 +0500

    Add Twig and Base template

--- index.php
+++ index.php
@@ -3,17 +3,24 @@ declare(strict_types=1);
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Twig\Environment;
+use \Twig\Loader\FilesystemLoader;
 
 require __DIR__ . '/vendor/autoload.php';
 
+$loader = new FilesystemLoader('templates');
+$view = new Environment($loader);
+
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
+$app->get('/', function (Request $request, Response $response, $args) use ($view){
+    $body = $view->render('index.twig');
+    $response->getBody()->write($body);
     return $response;
 });
-$app->get('/about', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello about!");
+$app->get('/about', function (Request $request, Response $response, $args) use($view){
+    $body = $view->render('about.twig');
+    $response->getBody()->write($body);
     return $response;
 });
 
